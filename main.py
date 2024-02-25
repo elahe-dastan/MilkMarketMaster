572cd185c5745c1f69518f03af411567afd0e9ff
Elahe Dastan <elahe.dstn@gmail.com>
2024-02-25 18:10:18 +0000

    feat: provide pipenv beside the docker and docker compose

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # main.py
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 import joblib
 import numpy as np
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from statsmodels.tsa.arima.model import ARIMA
 
 app = FastAPI()
 
 # Load the ARIMA model
-model = joblib.load('./arima_model.joblib')
+model = joblib.load("./arima_model.joblib")
 
 
 class PredictionInput(BaseModel):
