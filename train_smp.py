8268bea1ed190a344cad5df2357995aaecf58fab
Elahe Dastan <elahe.dstn@gmail.com>
2024-02-28 15:56:43 +0000

    feat: complete train_smp

--- train_smp.py
+++ train_smp.py
@@ -1,26 +1,37 @@
+import logging
+
 import joblib
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.arima.model import ARIMA
 
-
-def read():
-    smp_2_4 = pd.read_csv("./data/2/4/smp_quotations.csv")
-
-    smp_2_4 = smp_2_4.sort_values(by="date")
-
-    smp_2_4["date"] = pd.to_datetime(smp_2_4["date"])
-    smp_2_4 = smp_2_4.drop_duplicates(["date"])
-    smp_2_4["date_b"] = smp_2_4["date"]
-    smp_2_4.set_index("date", inplace=True)
-    smp_2_4 = smp_2_4.asfreq("7D", method="pad")
-
-    return smp_2_4
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger("main")
 
 
-def split(df):
-    train_size = int(len(df) * 0.9)  # 80% for training, adjust as needed
+def read() -> pd.DataFrame:
+    smp_df = pd.read_csv("./data/2/4/smp_quotations.csv")
+
+    smp_df = smp_df.sort_values(by="date")
+
+    smp_df["date"] = pd.to_datetime(smp_df["date"])
+    smp_df = smp_df.drop_duplicates(["date"])
+    smp_df.set_index("date", inplace=True)
+
+    smp_df = smp_df.asfreq("7D", method="pad")
+
+    return smp_df
+
+
+def split_train_test(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    train_size = int(len(df) * 0.9)
     train, test = df[:train_size], df[train_size:]
+    # these asserts are here to be compatible with type checking
+    # system
+    assert isinstance(train, pd.DataFrame)
+    assert isinstance(test, pd.DataFrame)
 
     return train, test
 
@@ -33,44 +44,41 @@ def train_model(train_dataset, p, d, q):
     return fitted_model
 
 
-def evaluate_model(fitted_model, test_dataset):
-    forecasts = fitted_model.forecast(32)
+def evaluate_model(model, test_df: pd.DataFrame):
+    forecasts = model.forecast(len(test_df))
     forecasts = pd.DataFrame(forecasts)
+    forecasts["date"] = forecasts.index
 
-    inflation = pd.read_csv("./data/2/inflation_rates.csv")
-    inflation = inflation[inflation["data_interval"] == "yearly"]
+    inflation: pd.DataFrame = pd.read_csv("./data/2/inflation_rates.csv")
+    # drop quarterly inflation rate to use yearly only
+    yearly_inflation = inflation[inflation["data_interval"] == "yearly"]
+    # these asserts are here to be compatible with type checking
+    # system
+    assert isinstance(yearly_inflation, pd.DataFrame)
+    inflation = yearly_inflation
     inflation = inflation.sort_values(by="date")
 
     inflation["date"] = pd.to_datetime(inflation["date"])
     inflation.set_index("date", inplace=True)
 
-    # Extract the year from the date index in df_weekly
     forecasts.index = pd.to_datetime(forecasts.index)
-    forecasts["year"] = forecasts.index.year
-    inflation["year"] = inflation.index.year
+    inflation.index = pd.to_datetime(inflation.index)
+    forecasts["year"] = forecasts.index.year  # type: ignore
+    inflation["year"] = inflation.index.year  # type: ignore
 
-    # Merge based on the condition: year in df_weekly should match the 'date' column in df_yearly
     merged_df = pd.merge(
         forecasts, inflation, left_on="year", right_on="year", how="inner"
     )
+    merged_df.set_index("date", inplace=True)
 
-    # Drop the 'year' column if you don't need it in the final merged dataframe
-    merged_df = merged_df.drop(columns=["year"])
-    print(merged_df)
-
-    mse = mean_squared_error(
-        merged_df["predicted_mean"],
-        forecasts["predicted_mean"].values
-        + (forecasts["predicted_mean"].values * merged_df["rate"] / 100),
+    forecasts["adjusted_price"] = merged_df["predicted_mean"] * (
+        1 + (merged_df["rate"] / 100)
     )
-    print(f"Mean Squared Error: {mse}")
 
+    merged_df = merged_df.drop(columns=["year"])
 
-data = read()
-train, test = split(data)
-
-
-model = train_model(train, 12, 2, 1)
+    forecasts_mse = mean_squared_error(test_df["price"], forecasts["adjusted_price"])
+    print(f"Forecast Mean Squared Error: {forecasts_mse}")
 
 
 def load_model(country_id, product_id):
@@ -82,9 +90,15 @@ def load_model(country_id, product_id):
     return model
 
 
-evaluate_model(model, test)
-# model = load_model(2, 4)
+if __name__ == "__main__":
+    data = read()
+    train, test = split_train_test(data)
 
-# # Save the fitted model to a file using joblib
-filename = "./data/2/4/smp_quotations_arima_model.joblib"
-joblib.dump(model, filename)
+    model = train_model(train, 12, 2, 1)
+
+    evaluate_model(model, test)
+    # model = load_model(2, 4)
+
+    # # Save the fitted model to a file using joblib
+    filename = "./data/2/4/smp_quotations_arima_model.joblib"
+    joblib.dump(model, filename)
