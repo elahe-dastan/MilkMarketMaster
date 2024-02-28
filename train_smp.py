7fce065b4991cc6cb408a164e0c47e3b3f5c2fb3
Elahe Dastan <elahe.dstn@gmail.com>
2024-02-28 10:27:48 +0000

    chore: provide freq into the arima model

--- train_smp.py
+++ train_smp.py
@@ -1,17 +1,17 @@
-from statsmodels.tsa.arima.model import ARIMA
-from sklearn.metrics import mean_squared_error
-import pandas as pd
 import joblib
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.arima.model import ARIMA
 
 
 def read():
-    smp_2_4 = pd.read_csv('./data/2/4/smp_quotations.csv')
+    smp_2_4 = pd.read_csv("./data/2/4/smp_quotations.csv")
 
-    smp_2_4 = smp_2_4.sort_values(by='date')
+    smp_2_4 = smp_2_4.sort_values(by="date")
 
-    smp_2_4['date'] = pd.to_datetime(smp_2_4['date'])
-    smp_2_4['date_b'] = smp_2_4['date']
-    smp_2_4.set_index('date', inplace=True)
+    smp_2_4["date"] = pd.to_datetime(smp_2_4["date"])
+    smp_2_4["date_b"] = smp_2_4["date"]
+    smp_2_4.set_index("date", inplace=True)
 
     return smp_2_4
 
@@ -25,7 +25,7 @@ def split(df):
 
 def train_model(train_dataset, p, d, q):
     order = (p, d, q)
-    model = ARIMA(train_dataset['price'], order=order)
+    model = ARIMA(train_dataset["price"], order=order, freq="W")
     fitted_model = model.fit()
 
     return fitted_model
@@ -35,31 +35,33 @@ def evaluate_model(fitted_model, test_dataset):
     predictions = fitted_model.predict(start=len(train), end=len(train) + len(test) - 1)
     forecasts = fitted_model.forecast(len(test))
 
-    inflation = pd.read_csv('./data/2/inflation_rates.csv')
-    inflation = inflation[inflation['data_interval'] == 'yearly']
-    inflation = inflation.sort_values(by='date')
+    inflation = pd.read_csv("./data/2/inflation_rates.csv")
+    inflation = inflation[inflation["data_interval"] == "yearly"]
+    inflation = inflation.sort_values(by="date")
 
-    inflation['date'] = pd.to_datetime(inflation['date'])
-    inflation['date_a'] = inflation['date']
-    inflation.set_index('date', inplace=True)
-
-
+    inflation["date"] = pd.to_datetime(inflation["date"])
+    inflation["date_a"] = inflation["date"]
+    inflation.set_index("date", inplace=True)
 
     # Extract the year from the date index in df_weekly
-    test_dataset['year'] = test_dataset.index.year
-    inflation['year'] = inflation.index.year
+    test_dataset["year"] = test_dataset.index.year
+    inflation["year"] = inflation.index.year
 
     # Merge based on the condition: year in df_weekly should match the 'date' column in df_yearly
-    merged_df = pd.merge(test_dataset, inflation, left_on='year', right_on='year', how='inner')
+    merged_df = pd.merge(
+        test_dataset, inflation, left_on="year", right_on="year", how="inner"
+    )
 
     # Drop the 'year' column if you don't need it in the final merged dataframe
-    merged_df = merged_df.drop(columns=['year'])
+    merged_df = merged_df.drop(columns=["year"])
 
-
-    mse = mean_squared_error(merged_df['price'], predictions.values + (predictions.values * merged_df['rate'] / 100))
-    forecasts_mse = mean_squared_error(test_dataset['price'], forecasts)
-    print(f'Mean Squared Error: {mse}')
-    print(f'Forecast Mean Squared Error: {forecasts_mse}')
+    mse = mean_squared_error(
+        merged_df["price"],
+        predictions.values + (predictions.values * merged_df["rate"] / 100),
+    )
+    forecasts_mse = mean_squared_error(test_dataset["price"], forecasts)
+    print(f"Mean Squared Error: {mse}")
+    print(f"Forecast Mean Squared Error: {forecasts_mse}")
 
 
 data = read()
